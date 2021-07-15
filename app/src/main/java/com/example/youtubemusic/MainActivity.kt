f9c1040c62ecda0700ef1e651055f2bfda763261
SoYeonKim522 <sykim096@gmail.com>
2021-07-16 02:17:21 +0900

    Build first page layout

---
+++ app/src/main/java/com/example/youtubemusic/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.youtubemusic
+
+import android.graphics.drawable.GradientDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
